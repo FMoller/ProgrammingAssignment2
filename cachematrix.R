541d78ac4b596b70e7597d42ab664e6d1eb05df9
FMoller <fredericomollerper@gmail.com>
2014-06-19 20:52:10 -0300

    cachematrix
    Update problem

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## First function create a cache for a matrix. 'm' is an objects for internal operations
+## and x and y objects to recieve and store the matrix.
+## get function returns the matrix stored, set function, recieve a matrix 'y' and store it in an internal
+## object x.
+## setsolve stores the inverse of stored matrix, getsolve returns it.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
+## cachesolve recieves an stored matrix/inveser. It reads the inverse
+## of this matrix, if its already stored, the function only return 
+## this stored inverse. Else, the function calculates and store
+## inverse function.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+  m<-x$getsolve()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  else{
+    matrix<-x$get()
+    m<-solve(matrix, ...)
+    x$setsolve(m)
+    return(m)
+  }
 }
+
+##Sorry for my very bad english.
